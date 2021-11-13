c15fe61137b8663b0c64b2334abd8bf1e2a7fbe3
JakeJackson99 <jakecjackson99@gmail.com>
2021-11-13 17:31:54 +0000

    Improve documentation

--- app/utility.py
+++ app/utility.py
@@ -1,8 +1,19 @@
+import requests
 STEAM32_ID = 197033655
+BASE_URL = "https://api.opendota.com/api"
 
-def wins_list(matches):
-    """Determine whether a user won a match in a list of matches. 
 
-    :return: list of boolean values corresponding to a win, i.e. True -> win
+def match_results(steam32_id, hero_id, num_results=20):
+    """Determine whether a user won a match from a list of matches. 
+
+    Args:
+        steam32_id -- id relating to user's Steam ID
+        hero_id -- id relating to a specific hero
+
+    Returns:
+        list of boolean values corresponding to a win, i.e. True -> win
     """
-    return [True if (match['player_slot'] < 128) ==  match['radiant_win'] else False for match in matches]
+    matches = requests.get(
+        f"https://api.opendota.com/api/players/{steam32_id}/matches?hero_id={hero_id}").json()[0:num_results]
+
+    return [True if (match['player_slot'] < 128) == match['radiant_win'] else False for match in matches]
